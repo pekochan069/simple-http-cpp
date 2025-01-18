3f9d73441d0ef843d91295108f4678a552caea2d
pekochan069 <pekochan069@gmail.com>
2025-01-19 01:22:35 +0900

    Add log functions

--- src/log.hpp
+++ src/log.hpp
@@ -3,5 +3,10 @@
 #ifdef _DEBUG
     #define LOG(...) std::println(__VA_ARGS__)
 #else
-    #define LOG(...) 
+    #define LOG(...)
 #endif
+
+#define TRACE(...) std::println(__VA_ARGS__)
+#define INFO(...) std::println(__VA_ARGS__)
+#define WARN(...) std::println(__VA_ARGS__)
+#define ERROR(...) std::println(__VA_ARGS__)
