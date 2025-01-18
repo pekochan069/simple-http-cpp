501454d397a125b9d813184c3bf0f07b8b6f9602
pekochan069 <pekochan069@gmail.com>
2025-01-19 01:34:47 +0900

    Change names of log functions

--- src/log.hpp
+++ src/log.hpp
@@ -1,12 +1,15 @@
 #include <print>
 
+namespace http {
 #ifdef _DEBUG
     #define LOG(...) std::println(__VA_ARGS__)
 #else
     #define LOG(...)
 #endif
 
-#define TRACE(...) std::println(__VA_ARGS__)
-#define INFO(...) std::println(__VA_ARGS__)
-#define WARN(...) std::println(__VA_ARGS__)
-#define ERROR(...) std::println(__VA_ARGS__)
+#define LOG_TRACE(...) std::println(__VA_ARGS__)
+#define LOG_INFO(...) std::println(__VA_ARGS__)
+#define LOG_WARN(...) std::println(__VA_ARGS__)
+#define LOG_ERROR(...) std::println(__VA_ARGS__)
+
+}
